ee208ea958ab5ade1eca2815bffaad4f3516cc96
= <=>
2025-07-04 10:24:45 +0100

    aactuaalizacao 011

--- src/app/pages/page-home/page-home.ts
+++ src/app/pages/page-home/page-home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ComponentHeader } from "../../components/component-header/component-header";
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.html',
-  styleUrl: './page-home.css'
+  styleUrl: './page-home.css',
+  imports: [ComponentHeader]
 })
 export class PageHome {
   constructor(private router: Router) {}
