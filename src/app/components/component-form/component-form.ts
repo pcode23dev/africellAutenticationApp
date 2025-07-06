22434f24945afc018766b81d502c68ec626d6b2e
= <=>
2025-07-06 17:59:57 +0100

    9876543

--- src/app/components/component-form/component-form.ts
+++ src/app/components/component-form/component-form.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -9,26 +9,33 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [ReactiveFormsModule]
 })
+
 export class ComponentForm {
-
-  @Output() aoContinuar = new EventEmitter<any>(); // Use 'any' para enviar todos os dados
-
+  @Output() aoContinuar = new EventEmitter<any>();
   formulario: FormGroup;
+  validado = signal<boolean>(true);
 
   constructor(private fb: FormBuilder, private router: Router) {
     this.formulario = this.fb.group({
-      nome: ['', Validators.required],
+      nome: ['', [Validators.required, Validators.pattern(/\S+/)]],
       apelido: [''],
-      biNumber: ['', Validators.required],
-      phone: ['', Validators.required],
-      email: ['']
+      biNumber: ['', [Validators.required, Validators.pattern(/^\d{9}[a-zA-Z]{2}\d{3}$/)]],
+      phone: ['', [Validators.required, Validators.pattern(/^[9][1-579]\d{7}$/)]],
+      email: ['', [Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]]
     });
+  }
+
+  campoInvalido(nomeCampo: string): boolean {
+    const ctrl = this.formulario.get(nomeCampo);
+    return !!ctrl && ctrl.touched && ctrl.invalid;
   }
 
   onSubmit() {
     if (this.formulario.valid) {
+      this.validado.set(true);
       this.aoContinuar.emit(this.formulario.value);
     } else {
+      this.validado.set(false);
       this.formulario.markAllAsTouched();
     }
   }
